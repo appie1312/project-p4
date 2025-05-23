7f401a4b3750940d454361095ca28c18e808fc4f
Your Name <you@example.com>
2025-05-23 09:16:29 +0200

    navbar

--- app/views/ticket/index.php
+++ app/views/ticket/index.php
@@ -1,3 +1,4 @@
+<?php require_once APPROOT . '/views/includes/navbar.php'; ?>
 <?php
 // Geen initialisatie van $tickets, $voorstellingen, $filters, $currentUser, $currentDateTime!
 // Alles komt uit de controller via extract($viewData);
